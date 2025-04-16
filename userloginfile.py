b3e316377b5dcf46c501f06ae9f2dc7e9b5442f0
senchelung <senchelungrai@gmail.com>
2025-04-16 11:28:05 +0545

    Make some comment

--- userloginfile.py
+++ userloginfile.py
@@ -10,3 +10,22 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+    def main():
+        root = tk.Tk()
+        root.title("user view")
+        root.geometry("600*800")
+
+        #username label and entry
+        tk.Label(root,text="username:").pack(pady=5)
+        #password label and entry
+        tk.Label(root,text="password:").pack(pady=5)
+        #login button
+        tk.Button(root,text="Login:").pack(pady=10)
+
+        root.mainloop()
+
+    if__name__=="__main__":
+    main()
+
